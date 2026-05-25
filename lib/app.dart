455b1f5b2ec5ce9c7d78f98bc76af5f66df0092f
hack <skek933@gmail.com>
2026-05-25 10:28:15 +0900

    앱 이름 아이콘 변경

--- lib/app.dart
+++ lib/app.dart
@@ -12,7 +12,7 @@ class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: '편안한 도구들',
+      title: 'ToolBox',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
         colorSchemeSeed: Colors.blue,
